b56022e422d062f1e48e61bf036bad65d20b5ace
Lindsay Wells <Lindsay.Wells@utas.edu.au>
2024-04-08 00:16:05 +1000

    solution

--- app/src/main/java/au/edu/utas/kit305/tutorial05/MainActivity.kt
+++ app/src/main/java/au/edu/utas/kit305/tutorial05/MainActivity.kt
@@ -1,6 +1,7 @@
 package au.edu.utas.kit305.tutorial05
 
 import android.annotation.SuppressLint
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -11,6 +12,10 @@ import au.edu.utas.kit305.tutorial05.databinding.ActivityMainBinding
 import au.edu.utas.kit305.tutorial05.databinding.MyListItemBinding
 import com.google.firebase.Firebase
 import com.google.firebase.firestore.firestore
+import com.google.firebase.firestore.toObject
+
+const val FIREBASE_TAG = "FirebaseLogging"
+const val MOVIE_INDEX = "Movie_Index"
 
 val items = mutableListOf<Movie>()
 
@@ -33,6 +38,35 @@ class MainActivity : AppCompatActivity()
         //get db connection
         val db = Firebase.firestore
         Log.d("FIREBASE", "Firebase connected: ${db.app.name}")
+
+        val moviesCollection = db.collection("movies")
+        ui.lblMovieCount.text = "Loading..."
+        moviesCollection
+            .get()
+            .addOnSuccessListener { result ->
+                items.clear() //this line clears the list, and prevents a bug where items would be duplicated upon rotation of screen
+                Log.d(FIREBASE_TAG, "--- all movies ---")
+                for (document in result)
+                {
+                    //Log.d(FIREBASE_TAG, document.toString())
+                    val movie = document.toObject<Movie>()
+                    movie.id = document.id
+                    Log.d(FIREBASE_TAG, movie.toString())
+
+                    items.add(movie)
+                }
+                //(ui.myList.adapter as MovieAdapter).notifyDataSetChanged()
+                //you may choose to fix the warning that notifyDataSetChanged() is not specific enough using:
+                (ui.myList.adapter as MovieAdapter).notifyItemRangeInserted(0, items.size)
+
+                ui.lblMovieCount.text = "${items.size} Movie(s)"
+            }
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        ui.myList.adapter?.notifyDataSetChanged() //without a more complicated set-up, we can't be more specific than "dataset changed"
     }
 
     inner class MovieHolder(var ui: MyListItemBinding) : RecyclerView.ViewHolder(ui.root) {}
@@ -50,7 +84,15 @@ class MainActivity : AppCompatActivity()
 
         override fun onBindViewHolder(holder: MainActivity.MovieHolder, position: Int) {
             val movie = movies[position]   //get the data at the requested position
+            val ui = holder.ui
+            ui.txtName.text = movie.title
+            ui.txtYear.text = movie.year.toString()
 
+            holder.ui.root.setOnClickListener {
+                val i = Intent(holder.ui.root.context, MovieDetails::class.java)
+                i.putExtra(MOVIE_INDEX, position)
+                startActivity(i)
+            }
         }
     }
 }
--- app/src/main/java/au/edu/utas/kit305/tutorial05/MovieDetails.kt
+++ app/src/main/java/au/edu/utas/kit305/tutorial05/MovieDetails.kt
@@ -2,7 +2,10 @@ package au.edu.utas.kit305.tutorial05
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import au.edu.utas.kit305.tutorial05.databinding.ActivityMovieDetailsBinding
+import com.google.firebase.Firebase
+import com.google.firebase.firestore.firestore
 
 class MovieDetails : AppCompatActivity() {
     private lateinit var ui : ActivityMovieDetailsBinding
@@ -11,6 +14,31 @@ class MovieDetails : AppCompatActivity() {
         ui = ActivityMovieDetailsBinding.inflate(layoutInflater)
         setContentView(ui.root)
 
-        //TODO: read in movie details and display on this screen
+        //get movie object using id from intent
+        val movieID = intent.getIntExtra(MOVIE_INDEX, -1)
+        val movieObject = items[movieID]
+        //TODO: you'll need to set txtTitle, txtYear, txtDuration yourself
+        ui.txtTitle.setText(movieObject.title)
+        ui.txtDuration.setText(movieObject.duration.toString())
+        ui.txtYear.setText(movieObject.year.toString())
+
+        val db = Firebase.firestore
+        val moviesCollection = db.collection("movies")
+
+        ui.btnSave.setOnClickListener {
+            //get the user input
+            movieObject.title = ui.txtTitle.text.toString()
+            movieObject.year = ui.txtYear.text.toString().toInt() //good code would check this is really an int
+            movieObject.duration = ui.txtDuration.text.toString().toFloat() //good code would check this is really a float
+
+            //update the database
+            moviesCollection.document(movieObject.id!!)
+                .set(movieObject)
+                .addOnSuccessListener {
+                    Log.d(FIREBASE_TAG, "Successfully updated movie ${movieObject.id}")
+                    //return to the list
+                    finish()
+                }
+        }
     }
 }
