8b78af5aecae908ada0f92de1317aa2e6a7f09b8
RoboidPog <146046581+RoboidPog@users.noreply.github.com>
2025-03-27 00:14:53 +1100

    part 3 complete

--- app/src/main/java/au/edu/utas/kit305/tutorial05/MainActivity.kt
+++ app/src/main/java/au/edu/utas/kit305/tutorial05/MainActivity.kt
@@ -3,11 +3,17 @@ package au.edu.utas.kit305.tutorial05
 import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.ViewGroup
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import au.edu.utas.kit305.tutorial05.databinding.ActivityMainBinding
 import au.edu.utas.kit305.tutorial05.databinding.MyListItemBinding
+import com.google.firebase.Firebase
+import com.google.firebase.firestore.firestore
+import com.google.firebase.firestore.toObject
+
+const val FIREBASE_TAG = "FirebaseLogging"
 
 val items = mutableListOf<Movie>()
 
@@ -26,6 +32,45 @@ class MainActivity : AppCompatActivity()
 
         //vertical list
         ui.myList.layoutManager = LinearLayoutManager(this)
+
+
+        //get db connection
+        val db = Firebase.firestore
+        Log.d("FIREBASE", "Firebase connected: ${db.app.name}")
+
+        val lotr = Movie(
+            title = "Lord of the Rings: Fellowship of the ring",
+            year = 2001,
+            duration = 9001F
+        )
+
+        val moviesCollection = db.collection("movies")
+        /*moviesCollection
+            .add(lotr)
+            .addOnSuccessListener {
+                Log.d(FIREBASE_TAG, "document created with id ${it.id}")
+                lotr.id = it.id
+            }
+            .addOnFailureListener {
+                Log.e(FIREBASE_TAG, "error writing document", it)
+            }*/
+
+        ui.lblMovieCount.text = "Loading..."
+        moviesCollection
+            .get()
+            .addOnSuccessListener { result ->
+                items.clear()
+                Log.d(FIREBASE_TAG, "--- all movies ---")
+                for (document in result)
+                {
+                    val movie = document.toObject<Movie>()
+                    movie.id = document.id
+                    Log.d(FIREBASE_TAG, movie.toString())
+
+                    items.add(movie)
+                }
+                (ui.myList.adapter as MovieAdapter).notifyDataSetChanged()
+            }
     }
 
     inner class MovieHolder(var ui: MyListItemBinding) : RecyclerView.ViewHolder(ui.root) {}
@@ -43,7 +88,10 @@ class MainActivity : AppCompatActivity()
 
         override fun onBindViewHolder(holder: MainActivity.MovieHolder, position: Int) {
             val movie = movies[position]   //get the data at the requested position
+            holder.ui.txtName.text = movie.title
+            holder.ui.txtYear.text = movie.year.toString()
 
+            ui.lblMovieCount.text = "${getItemCount()} Movie(s)"
         }
     }
 }
